bfe59f7f98bff417341c65148789d32198c00059
jub0bs <jcretel-infosec+github@protonmail.com>
2024-09-20 09:57:20 +0200

    add various functions adapted from the slices pkg
    Equal, Compare, IsSorted, Min, Max, and their Func variants

--- seq.go
+++ seq.go
@@ -1,6 +1,7 @@
 package iterutil
 
 import (
+	"cmp"
 	"iter"
 )
 
@@ -188,6 +189,40 @@ func At[E any](seq iter.Seq[E], n int) (e E, ok bool) {
 	return
 }
 
+// Equal reports whether two iterators are equal:
+// the same length and all elements equal.
+// If the lengths are different, Equal returns false.
+// Otherwise, the elements are compared sequentially,
+// and the comparison stops at the first unequal pair.
+// Floating point NaNs are not considered equal.
+func Equal[E comparable](seq1, seq2 iter.Seq[E]) bool {
+	return EqualFunc(seq1, seq2, equal)
+}
+
+func equal[E comparable](e1, e2 E) bool { return e1 == e2 }
+
+// EqualFunc reports whether two iterators are equal using eq as equality
+// function on each pair of elements.
+// If the lengths are different, EqualFunc returns false.
+// Otherwise, the elements are compared sequentially,
+// and the comparison stops at the first pair for which eq returns false.
+func EqualFunc[A, B comparable](seq1 iter.Seq[A], seq2 iter.Seq[B], eq func(A, B) bool) bool {
+	next1, stop1 := iter.Pull(seq1)
+	defer stop1()
+	next2, stop2 := iter.Pull(seq2)
+	defer stop2()
+	for {
+		v1, ok1 := next1()
+		v2, ok2 := next2()
+		if !ok1 {
+			return !ok2
+		}
+		if ok1 != ok2 || !eq(v1, v2) {
+			return false
+		}
+	}
+}
+
 // Contains report whether target is present in seq.
 func Contains[E comparable](seq iter.Seq[E], target E) bool {
 	for e := range seq {
@@ -206,6 +241,161 @@ func ContainsFunc[E comparable](seq iter.Seq[E], p func(E) bool) bool {
 		}
 	}
 	return false
+}
+
+// Min, if seq is not empty, returns the minimal value in seq and true;
+// otherwise, it returns the zero value and false.
+// For floating-point numbers, Min propagates NaNs
+// (any NaN value in seq forces the output to be NaN).
+func Min[E cmp.Ordered](seq iter.Seq[E]) (E, bool) {
+	var (
+		m         E
+		firstSeen bool
+	)
+	for e := range seq {
+		if !firstSeen {
+			m = e
+			firstSeen = true
+			continue
+		}
+		m = min(e, m)
+	}
+	return m, firstSeen
+}
+
+// MinFunc, if seq is not empty, returns the minimal value
+// (using cmp as comparison function) in seq and true;
+// otherwise, it returns the zero value and false.
+// If there is more than one minimal element according
+// to the cmp function, MinFunc returns the first one.
+func MinFunc[E any](seq iter.Seq[E], cmp func(E, E) int) (E, bool) {
+	var (
+		m         E
+		firstSeen bool
+	)
+	for e := range seq {
+		if !firstSeen {
+			m = e
+			firstSeen = true
+			continue
+		}
+		if cmp(e, m) < 0 {
+			m = e
+		}
+	}
+	return m, firstSeen
+}
+
+// Max, if seq is not empty, returns the maximal value in seq and true;
+// otherwise, it returns the zero value and false.
+// For floating-point numbers, Max propagates NaNs
+// (any NaN value in seq forces the output to be NaN).
+func Max[E cmp.Ordered](seq iter.Seq[E]) (E, bool) {
+	var (
+		m        E
+		nonEmpty bool
+	)
+	for e := range seq {
+		nonEmpty = true
+		m = max(e, m)
+	}
+	return m, nonEmpty
+}
+
+// MaxFunc, if seq is not empty, returns the maximal value
+// (using cmp as comparison function) in seq and true;
+// otherwise, it returns the zero value and false.
+// If there is more than one maximal element according
+// to the cmp function, MaxFunc returns the first one.
+func MaxFunc[E any](seq iter.Seq[E], cmp func(E, E) int) (E, bool) {
+	var (
+		m        E
+		nonEmpty bool
+	)
+	for e := range seq {
+		nonEmpty = true
+		if cmp(e, m) > 0 {
+			m = e
+		}
+	}
+	return m, nonEmpty
+}
+
+// Compare compares the elements of seq1 and seq2,
+// using [cmp.Compare] on each pair of elements.
+// The elements are compared sequentially until one element is not equal to
+// the other.
+// The result of comparing the first non-matching elements is returned.
+// If seq1 and seq2 are equal until one of them ends,
+// the shorter one is considered less than the longer one.
+// The result is 0 if seq1 == seq2, -1 if seq1 < seq2, and +1 if seq1 > seq2.
+// For floating-point types, a NaN is considered less than any non-NaN,
+// and -0.0 is not less than (is equal to) 0.0.
+func Compare[E cmp.Ordered](seq1, seq2 iter.Seq[E]) int {
+	return CompareFunc(seq1, seq2, cmp.Compare)
+}
+
+// CompareFunc is like [Compare] but uses a custom comparison function on each
+// pair of elements.
+// The result is the first non-zero result of cmp;
+// if cmp always returns 0, the result is 0 if len(seq1) == len(seq2),
+// -1 if len(seq1) < len(seq2), and +1 if len(seq1) > len(seq2).
+func CompareFunc[A, B any](seq1 iter.Seq[A], seq2 iter.Seq[B], cmp func(A, B) int) int {
+	next1, stop1 := iter.Pull(seq1)
+	defer stop1()
+	next2, stop2 := iter.Pull(seq2)
+	defer stop2()
+	for {
+		v1, ok1 := next1()
+		v2, ok2 := next2()
+		switch {
+		case !ok1 && ok2:
+			return -1
+		case !ok1 && !ok2:
+			return 0
+		case ok1 && !ok2:
+			return 1
+		default:
+			if c := cmp(v1, v2); c != 0 {
+				return c
+			}
+		}
+	}
+}
+
+// IsSorted reports whether seq is sorted in ascending order.
+// For floating-point types, a NaN is considered less than any non-NaN,
+// and -0.0 is not less than (is equal to) 0.0.
+func IsSorted[E cmp.Ordered](seq iter.Seq[E]) bool {
+	var (
+		last      E
+		firstSeen bool
+	)
+	for e := range seq {
+		if firstSeen && cmp.Less(e, last) {
+			return false
+		}
+		last = e
+		firstSeen = true
+	}
+	return true
+}
+
+// IsSortedFunc reports whether seq is sorted in ascending order,
+// using cmp as comparison function.
+func IsSortedFunc[E any](seq iter.Seq[E], cmp func(E, E) int) bool {
+	var (
+		last      E
+		firstSeen bool
+	)
+	for e := range seq {
+		if firstSeen && cmp(e, last) < 0 {
+			return false
+		}
+		last = e
+		firstSeen = true
+	}
+	return true
 }
 
 // Foldl performs a [left-associative] [fold] of seq using
--- seq_test.go
+++ seq_test.go
@@ -1,9 +1,11 @@
 package iterutil_test
 
 import (
+	"cmp"
 	"fmt"
 	"iter"
 	"slices"
+	"strings"
 	"testing"
 
 	"github.com/jub0bs/iterutil"
@@ -526,6 +528,59 @@ func TestAt(t *testing.T) {
 	}
 }
 
+func ExampleEqual() {
+	seq1 := slices.Values([]string{"foo", "bar", "baz", "qux"})
+	seq2 := slices.Values([]string{"foo", "bar", "baz", "qux"})
+	fmt.Println(iterutil.Equal(seq1, seq2))
+	// Output:
+	// true
+}
+
+func TestEqual(t *testing.T) {
+	cases := []struct {
+		desc string
+		seq1 []string
+		seq2 []string
+		want bool
+	}{
+		{
+			desc: "equal",
+			seq1: []string{"foo", "bar", "baz"},
+			seq2: []string{"foo", "bar", "baz"},
+			want: true,
+		}, {
+			desc: "not same size",
+			seq1: []string{"foo", "bar", "baz", "qux"},
+			seq2: []string{"foo", "bar", "baz"},
+			want: false,
+		}, {
+			desc: "same size different values",
+			seq1: []string{"foo", "bar", "baz", "qux"},
+			seq2: []string{"foo", "bar", "baz", "quux"},
+			want: false,
+		},
+	}
+	for _, tc := range cases {
+		f := func(t *testing.T) {
+			seq1 := slices.Values(tc.seq1)
+			seq2 := slices.Values(tc.seq2)
+			got := iterutil.Equal(seq1, seq2)
+			if got != tc.want {
+				t.Errorf("got %t; want %t", got, tc.want)
+			}
+		}
+		t.Run(tc.desc, f)
+	}
+}
+
+func ExampleEqualFunc() {
+	seq1 := slices.Values([]string{"foo", "bar", "baz", "qux"})
+	seq2 := slices.Values([]string{"foO", "bAr", "Baz", "QUX"})
+	fmt.Println(iterutil.EqualFunc(seq1, seq2, strings.EqualFold))
+	// Output:
+	// true
+}
+
 func ExampleContains() {
 	seq := slices.Values([]int{1})
 	fmt.Println(iterutil.Contains(seq, 2))
@@ -545,6 +600,177 @@ func ExampleContainsFunc() {
 	// Output:
 	// false
 	// true
+}
+
+func ExampleMin() {
+	seq := slices.Values([]int(nil))
+	fmt.Println(iterutil.Min(seq))
+	seq = slices.Values([]int{3, 5, 1, 42})
+	fmt.Println(iterutil.Min(seq))
+	// Output:
+	// 0 false
+	// 1 true
+}
+
+func ExampleMinFunc() {
+	lenCmp := func(s1, s2 string) int { return cmp.Compare(len(s1), len(s2)) }
+	seq := slices.Values([]string(nil))
+	fmt.Println(iterutil.MinFunc(seq, lenCmp))
+	seq = slices.Values([]string{"quux", "qux", "baz", "bar", "foo"})
+	fmt.Println(iterutil.MinFunc(seq, lenCmp))
+	// Output:
+	//  false
+	// qux true
+}
+
+func ExampleMax() {
+	seq := slices.Values([]int(nil))
+	fmt.Println(iterutil.Max(seq))
+	seq = slices.Values([]int{3, 5, 1, 42})
+	fmt.Println(iterutil.Max(seq))
+	// Output:
+	// 0 false
+	// 42 true
+}
+
+func ExampleMaxFunc() {
+	lenCmp := func(s1, s2 string) int { return cmp.Compare(len(s1), len(s2)) }
+	seq := slices.Values([]string(nil))
+	fmt.Println(iterutil.MaxFunc(seq, lenCmp))
+	seq = slices.Values([]string{"qux", "quux", "corge", "grault", "garply"})
+	fmt.Println(iterutil.MaxFunc(seq, lenCmp))
+	// Output:
+	//  false
+	// grault true
+}
+
+func ExampleCompare() {
+	seq1 := slices.Values([]string{"foo", "bar", "baz", "qux"})
+	seq2 := slices.Values([]string{"foo", "bar", "baz", "qux", "quux"})
+	fmt.Println(iterutil.Compare(seq1, seq2))
+	// Output:
+	// -1
+}
+
+func TestCompare(t *testing.T) {
+	cases := []struct {
+		desc string
+		seq1 []string
+		seq2 []string
+		want int
+	}{
+		{
+			desc: "equal",
+			seq1: []string{"foo", "bar", "baz"},
+			seq2: []string{"foo", "bar", "baz"},
+			want: 0,
+		}, {
+			desc: "seq1 strict prefix of seq2",
+			seq1: []string{"foo", "bar", "baz"},
+			seq2: []string{"foo", "bar", "baz", "qux"},
+			want: -1,
+		}, {
+			desc: "seq2 strict prefix of seq1",
+			seq1: []string{"foo", "bar", "baz", "qux"},
+			seq2: []string{"foo", "bar", "baz"},
+			want: 1,
+		}, {
+			desc: "same values but last one",
+			seq1: []string{"foo", "bar", "baz", "qux"},
+			seq2: []string{"foo", "bar", "baz", "quux"},
+			want: 1,
+		},
+	}
+	for _, tc := range cases {
+		f := func(t *testing.T) {
+			seq1 := slices.Values(tc.seq1)
+			seq2 := slices.Values(tc.seq2)
+			got := iterutil.Compare(seq1, seq2)
+			if got != tc.want {
+				t.Errorf("got %d; want %d", got, tc.want)
+			}
+		}
+		t.Run(tc.desc, f)
+	}
+}
+
+func ExampleCompareFunc() {
+	seq1 := slices.Values([]string{"foo", "bar", "baz", "qux", "quux"})
+	seq2 := slices.Values([]string{"000", "111", "222", "333", "4444"})
+	lenCmp := func(s1, s2 string) int { return cmp.Compare(len(s1), len(s2)) }
+	fmt.Println(iterutil.CompareFunc(seq1, seq2, lenCmp))
+	// Output:
+	// 0
+}
+
+func ExampleIsSorted() {
+	seq := slices.Values([]string{"bar", "baz", "foo", "quux", "qux"})
+	fmt.Println(iterutil.IsSorted(seq))
+	// Output:
+	// true
+}
+
+func TestIsSorted(t *testing.T) {
+	cases := []struct {
+		desc  string
+		elems []string
+		want  bool
+	}{
+		{
+			desc:  "not sorted",
+			elems: []string{"foo", "bar", "baz", "qux", "quux"},
+			want:  false,
+		}, {
+			desc:  "sorted",
+			elems: []string{"bar", "baz", "foo", "quux", "qux"},
+			want:  true,
+		},
+	}
+	for _, tc := range cases {
+		f := func(t *testing.T) {
+			seq := slices.Values(tc.elems)
+			got := iterutil.IsSorted(seq)
+			if got != tc.want {
+				t.Errorf("got %t; want %t", got, tc.want)
+			}
+		}
+		t.Run(tc.desc, f)
+	}
+}
+
+func ExampleIsSortedFunc() {
+	seq := slices.Values([]string{"bar", "baz", "foo", "qux", "quux"})
+	lenCmp := func(s1, s2 string) int { return cmp.Compare(len(s1), len(s2)) }
+	fmt.Println(iterutil.IsSortedFunc(seq, lenCmp))
+	// Output:
+	// true
+}
+
+func TestIsSortedFunc(t *testing.T) {
+	cases := []struct {
+		desc  string
+		elems []string
+	}{
+		{
+			desc:  "not sorted",
+			elems: []string{"bar", "baz", "foo", "quux", "qux"},
+		}, {
+			desc:  "sorted",
+			elems: []string{"bar", "baz", "foo", "qux", "quux"},
+		},
+	}
+	for _, tc := range cases {
+		f := func(t *testing.T) {
+			lenCmp := func(s1, s2 string) int { return cmp.Compare(len(s1), len(s2)) }
+			seq := slices.Values(tc.elems)
+			got := iterutil.IsSortedFunc(seq, lenCmp)
+			want := slices.IsSortedFunc(tc.elems, lenCmp)
+			if got != want {
+				t.Errorf("got %t; want %t", got, want)
+			}
+		}
+		t.Run(tc.desc, f)
+	}
 }
 
 func ExampleFoldl() {
