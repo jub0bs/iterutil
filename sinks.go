2e2161edbfe6a60855985c24541d8752e01da3d7
jub0bs <jcretel-infosec+github@protonmail.com>
2024-09-21 10:52:58 +0200

    warn about sinks that may not terminate

--- sinks.go
+++ sinks.go
@@ -49,6 +49,7 @@ func At[E any](seq iter.Seq[E], n int) (e E, ok bool) {
 // Otherwise, the elements are compared sequentially,
 // and the comparison stops at the first unequal pair.
 // Floating point NaNs are not considered equal.
+// Equal may not terminate if seq1 or seq2 or both are infinite.
 func Equal[E comparable](seq1, seq2 iter.Seq[E]) bool {
 	return EqualFunc(seq1, seq2, equal)
 }
@@ -60,6 +61,7 @@ func equal[E comparable](e1, e2 E) bool { return e1 == e2 }
 // If the lengths are different, EqualFunc returns false.
 // Otherwise, the elements are compared sequentially,
 // and the comparison stops at the first pair for which eq returns false.
+// EqualFunc may not terminate if seq1 or seq2 or both are infinite.
 func EqualFunc[A, B comparable](seq1 iter.Seq[A], seq2 iter.Seq[B], eq func(A, B) bool) bool {
 	next1, stop1 := iter.Pull(seq1)
 	defer stop1()
@@ -78,6 +80,7 @@ func EqualFunc[A, B comparable](seq1 iter.Seq[A], seq2 iter.Seq[B], eq func(A, B
 }
 
 // Contains report whether target is present in seq.
+// It may not terminate if seq is infinite.
 func Contains[E comparable](seq iter.Seq[E], target E) bool {
 	for e := range seq {
 		if e == target {
@@ -88,6 +91,7 @@ func Contains[E comparable](seq iter.Seq[E], target E) bool {
 }
 
 // ContainsFunc reports whether at least one element e of seq satisfies p(e).
+// It may not terminate if seq is infinite.
 func ContainsFunc[E any](seq iter.Seq[E], p func(E) bool) bool {
 	for e := range seq {
 		if p(e) {
@@ -101,6 +105,7 @@ func ContainsFunc[E any](seq iter.Seq[E], p func(E) bool) bool {
 // otherwise, it returns the zero value and false.
 // For floating-point numbers, Min propagates NaNs
 // (any NaN value in seq forces the output to be NaN).
+// Min terminates if and only if seq is finite.
 func Min[E cmp.Ordered](seq iter.Seq[E]) (E, bool) {
 	var (
 		m         E
@@ -122,6 +127,7 @@ func Min[E cmp.Ordered](seq iter.Seq[E]) (E, bool) {
 // otherwise, it returns the zero value and false.
 // If there is more than one minimal element according
 // to the cmp function, MinFunc returns the first one.
+// MinFunc terminates if and only if seq is finite.
 func MinFunc[E any](seq iter.Seq[E], cmp func(E, E) int) (E, bool) {
 	var (
 		m         E
@@ -144,6 +150,7 @@ func MinFunc[E any](seq iter.Seq[E], cmp func(E, E) int) (E, bool) {
 // otherwise, it returns the zero value and false.
 // For floating-point numbers, Max propagates NaNs
 // (any NaN value in seq forces the output to be NaN).
+// Max terminates if and only if seq is finite.
 func Max[E cmp.Ordered](seq iter.Seq[E]) (E, bool) {
 	var (
 		m        E
@@ -161,6 +168,7 @@ func Max[E cmp.Ordered](seq iter.Seq[E]) (E, bool) {
 // otherwise, it returns the zero value and false.
 // If there is more than one maximal element according
 // to the cmp function, MaxFunc returns the first one.
+// MaxFunc terminates if and only if seq is finite.
 func MaxFunc[E any](seq iter.Seq[E], cmp func(E, E) int) (E, bool) {
 	var (
 		m        E
@@ -185,6 +193,7 @@ func MaxFunc[E any](seq iter.Seq[E], cmp func(E, E) int) (E, bool) {
 // The result is 0 if seq1 == seq2, -1 if seq1 < seq2, and +1 if seq1 > seq2.
 // For floating-point types, a NaN is considered less than any non-NaN,
 // and -0.0 is not less than (is equal to) 0.0.
+// It may not terminate if seq1 or seq2 or both are infinite.
 func Compare[E cmp.Ordered](seq1, seq2 iter.Seq[E]) int {
 	return CompareFunc(seq1, seq2, cmp.Compare)
 }
@@ -194,6 +203,7 @@ func Compare[E cmp.Ordered](seq1, seq2 iter.Seq[E]) int {
 // The result is the first non-zero result of cmp;
 // if cmp always returns 0, the result is 0 if len(seq1) == len(seq2),
 // -1 if len(seq1) < len(seq2), and +1 if len(seq1) > len(seq2).
+// It may not terminate if seq1 or seq2 or both are infinite.
 func CompareFunc[A, B any](seq1 iter.Seq[A], seq2 iter.Seq[B], cmp func(A, B) int) int {
 	next1, stop1 := iter.Pull(seq1)
 	defer stop1()
@@ -220,6 +230,7 @@ func CompareFunc[A, B any](seq1 iter.Seq[A], seq2 iter.Seq[B], cmp func(A, B) in
 // IsSorted reports whether seq is sorted in ascending order.
 // For floating-point types, a NaN is considered less than any non-NaN,
 // and -0.0 is not less than (is equal to) 0.0.
+// It may not terminate if seq is infinite.
 func IsSorted[E cmp.Ordered](seq iter.Seq[E]) bool {
 	var (
 		last      E
@@ -237,6 +248,7 @@ func IsSorted[E cmp.Ordered](seq iter.Seq[E]) bool {
 
 // IsSortedFunc reports whether seq is sorted in ascending order,
 // using cmp as comparison function.
+// It may not terminate if seq is infinite.
 func IsSortedFunc[E any](seq iter.Seq[E], cmp func(E, E) int) bool {
 	var (
 		last      E
@@ -255,6 +267,7 @@ func IsSortedFunc[E any](seq iter.Seq[E], cmp func(E, E) int) bool {
 // Foldl performs a [left-associative] [fold] of seq using
 // b as the initial value and
 // f as the left-associative binary operation.
+// It terminates if and only if seq is finite.
 //
 // [fold]: https://en.wikipedia.org/wiki/Fold_(higher-order_function)
 // [left-associative]: https://en.wikipedia.org/wiki/Associative_property#Notation_for_non-associative_operations
