29bd95c10138a3378dae2f6672b4a7a23a2a4db3
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-03 10:05:36 +0200

    remove AllErrors
    The semantics of traversing an error tree need to be fleshed out.

--- sources.go
+++ sources.go
@@ -137,37 +137,3 @@ func keys[K comparable, V any](m map[K]V) []K {
 	}
 	return ks
 }
-
-// AllErrors performs a [pre-order traversal] of err and returns an iterator
-// over its index-error pairs. For more context, see the [errors] package.
-//
-// [pre-order traversal]: https://en.wikipedia.org/wiki/Tree_traversal#Arbitrary_trees
-func AllErrors(err error) iter.Seq2[int, error] {
-	return func(yield func(int, error) bool) {
-		var i int
-		if !yield(i, err) {
-			return
-		}
-		i++
-		switch err := err.(type) {
-		case interface{ Unwrap() []error }:
-			for _, err := range err.Unwrap() {
-				for _, err := range AllErrors(err) {
-					if !yield(i, err) {
-						return
-					}
-					i++
-				}
-			}
-		case interface{ Unwrap() error }:
-			for _, err := range AllErrors(err.Unwrap()) {
-				if !yield(i, err) {
-					return
-				}
-				i++
-			}
-		default:
-			return
-		}
-	}
-}
